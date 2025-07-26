5cd1ca127f3d87be79902635ecf516262b36bdfd
Marina Panda <marinapanda0@gmail.com>
2025-07-27 00:12:37 +0530

    Adding Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 const App = () => {
   return <div>
     <Header />
     <Main />
+    <Footer />
   </div>
 };
 
